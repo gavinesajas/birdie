88a33f52d42fc68dd02721b95e1b2ad43155a90e
Gavin Esajas <cajunfabrics@gavinmacbook.local>
2016-07-18 22:58:00 +0200

    Changed userName value

--- SetupGit/ViewController.swift
+++ SetupGit/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let userName = "dummyUser"
+    let userName = "dummyUser2"
 
     override func viewDidLoad() {
         super.viewDidLoad()
